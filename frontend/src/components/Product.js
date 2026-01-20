4d184779d2bbd31ff2fd5ac1cb4bf69de6fcb57b
Benish-16 <sheikhbenish11@gmail.com>
2026-01-20 19:19:42 +0530

    Update Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -12,7 +12,7 @@ export default function Product({ category }) {
     const fetchTypes = async () => {
       try {
         const res = await fetch(
-          `https://clothing-store-new.onrender.com//api/type/category/${category}`
+          `https://clothing-store-new.onrender.com/api/type/category/${category}`
         );
         const data = await res.json();
         setTypes(data);
