089bd977e34c8e69e22087cd56ebc0e411d2e98a
Benish-16 <sheikhbenish11@gmail.com>
2026-01-20 16:34:06 +0530

    Update CartState.js

--- frontend/src/context/cart/CartState.js
+++ frontend/src/context/cart/CartState.js
@@ -1,5 +1,5 @@
 
-import React, { createContext, useContext, useState, useEffect } from "react";
+import React, {  useContext, useState, useEffect } from "react";
 
 import CartContext from "./CartContext";
 
