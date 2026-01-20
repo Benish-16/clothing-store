3402aeead95be73dfad5374f84451db1440d2179
Benish-16 <sheikhbenish11@gmail.com>
2026-01-20 19:14:07 +0530

    Update ProductItemsPage.js

--- frontend/src/pages/ProductItemsPage.js
+++ frontend/src/pages/ProductItemsPage.js
@@ -16,7 +16,7 @@ const { user } = useContext(authContext);
     const fetchProducts = async () => {
       try {
         const res = await fetch(
-        `http://localhost:5000/api/product/fetchproduct?category=${category}&type=${type}`
+        `https://clothing-store-new.onrender.com/api/product/fetchproduct?category=${category}&type=${type}`
         );
         const data = await res.json();
         if (data.success) {
