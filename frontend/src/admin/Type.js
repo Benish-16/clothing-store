630dc308b4f76b29f713dadec4ccc835a973c4c5
Benish-16 <sheikhbenish11@gmail.com>
2026-01-21 12:31:31 +0530

    Update Type.js

--- frontend/src/admin/Type.js
+++ frontend/src/admin/Type.js
@@ -32,7 +32,7 @@ export default function AddType() {
     };
 
     try {
-      const res = await fetch("https://clothing-store-backc.onrender.com/api/type/add", {
+      const res = await fetch("https://clothing-store-backc-p6nl.onrender.com/api/type/add", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload)
