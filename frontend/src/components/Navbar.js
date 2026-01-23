095809ed02d37c59003bbcb821fffff4ee98a14e
Benish-16 <sheikhbenish11@gmail.com>
2026-01-23 12:23:26 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -93,58 +93,64 @@ export default function Navbar() {
         )}
 
       
-        <div className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`}>
-          <ul className="navbar-nav text-center py-4 gap-3">
+       <nav className="navbar navbar-expand-lg navbar-light bg-white px-3">
+  <div className="container-fluid d-flex align-items-center">
 
-            <li><Link to="/women" className="nav-link" onClick={() => setMenuOpen(false)}>WOMEN</Link></li>
-            <li><Link to="/men" className="nav-link" onClick={() => setMenuOpen(false)}>MEN</Link></li>
-            <li><Link to="/about" className="nav-link" onClick={() => setMenuOpen(false)}>ABOUT</Link></li>
-            <li><Link to="/contact" className="nav-link" onClick={() => setMenuOpen(false)}>CONTACT</Link></li>
+    {/* 👜 Bag – LEFT */}
+    <Link to="/cart" className="nav-link order-1">
+      <i className="bi bi-bag fs-4"></i>
+    </Link>
 
-            {!localStorage.getItem("token") ? (
-              <>
-                <li className="nav-item">
-                  <Link
-                    to="/login"
-                    className="btn btn-outline-dark w-75 mx-auto"
-                    onClick={() => setMenuOpen(false)}
-                  >
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    to="/signup"
-                    className="btn btn-dark w-75 mx-auto"
-                    onClick={() => setMenuOpen(false)}
-                  >
-                    Signup
-                  </Link>
-                </li>
-              </>
-            ) : (
-              <li className="nav-item">
-                <button
-                  className="btn btn-outline-dark w-75 mx-auto"
-                  onClick={() => {
-                    handleLogout();
-                    setMenuOpen(false);
-                  }}
-                >
-                  Logout
-                </button>
-              </li>
-            )}
+    {/* ☰ Hamburger – RIGHT */}
+    <button
+      className="navbar-toggler order-3"
+      type="button"
+      onClick={() => setMenuOpen(!menuOpen)}
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
-            <li>
-              <Link to="/cart" className="nav-link" onClick={() => setMenuOpen(false)}>
-                <i className="bi bi-bag"></i>
+    {/* Collapsible menu */}
+    <div
+      className={`collapse navbar-collapse order-2 ${menuOpen ? "show" : ""}`}
+    >
+      <ul className="navbar-nav text-center py-4 gap-3 ms-auto">
+        <li><Link to="/women" className="nav-link" onClick={() => setMenuOpen(false)}>WOMEN</Link></li>
+        <li><Link to="/men" className="nav-link" onClick={() => setMenuOpen(false)}>MEN</Link></li>
+        <li><Link to="/about" className="nav-link" onClick={() => setMenuOpen(false)}>ABOUT</Link></li>
+        <li><Link to="/contact" className="nav-link" onClick={() => setMenuOpen(false)}>CONTACT</Link></li>
+
+        {!localStorage.getItem("token") ? (
+          <>
+            <li className="nav-item">
+              <Link to="/login" className="btn btn-outline-dark w-75 mx-auto" onClick={() => setMenuOpen(false)}>
+                Login
               </Link>
             </li>
+            <li className="nav-item">
+              <Link to="/signup" className="btn btn-dark w-75 mx-auto" onClick={() => setMenuOpen(false)}>
+                Signup
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li className="nav-item">
+            <button
+              className="btn btn-outline-dark w-75 mx-auto"
+              onClick={() => {
+                handleLogout();
+                setMenuOpen(false);
+              }}
+            >
+              Logout
+            </button>
+          </li>
+        )}
+      </ul>
+    </div>
 
-          </ul>
-        </div>
-      </div>
-    </nav>
+  </div>
+</nav>
+
   );
 }
